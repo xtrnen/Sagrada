62bf3c8e1353e67a46b3b8e8b376be336c1a6733
xtrnen <jtrnenyx@gmail.com>
2020-02-05 19:02:03 +0100

    Declaration of primary rule functions

--- app/src/main/java/Model/RuleHandler.java
+++ app/src/main/java/Model/RuleHandler.java
@@ -1,4 +1,39 @@
 package Model;
 
 public class RuleHandler {
+    /*
+    * RULES:
+    * One dice has to be in corner or edge position
+    * Each dice has at least one neighbor in any direction
+    * Dice has to fullfill condition of given slot (color / number)
+    * Edge of dice cannot has neighbor of the same color / number
+    * */
+    /*
+    * private array of pattern
+    * private array of dices*/
+    public Boolean EdgeNeighborRule(/*row and col of suggested dice*/)
+    {
+        return true;
+    }
+    public Boolean SlotConditionRule(/*row & col*/)
+    {
+        return true;
+    }
+    public Boolean IsNeighborRule(/*row & col*/)
+    {
+        return true;
+    }
+    public Boolean IsEdgeDiceRule()
+    {
+        return true;
+    }
+    private Boolean SlotColorRule()
+    {
+        return true;
+    }
+    private Boolean SlotNumberRule()
+    {
+        return true;
+    }
+    //private array GetNeighbors()
 }
