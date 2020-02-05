cf4023d62ef18aae65b7cd3b2d9a19e4a3de5d05
xtrnen <jtrnenyx@gmail.com>
2020-02-05 19:12:06 +0100

    Declaration of primary point calculation function

--- app/src/main/java/Model/RuleHandler.java
+++ app/src/main/java/Model/RuleHandler.java
@@ -11,6 +11,17 @@ public class RuleHandler {
     /*
     * private array of pattern
     * private array of dices*/
+    /*
+    * POINTS:
+    * Group assigns:
+    *   each set of dices that fit the assign counts
+    * Personal assign:
+    *   each card has specified counting
+    * Craftsman stones:
+    *   each not used stone counts for 1P
+    * Empty slots:
+    *   for each empty slot lose 1P
+    * */
     public Boolean EdgeNeighborRule(/*row and col of suggested dice*/)
     {
         return true;
@@ -27,6 +38,11 @@ public class RuleHandler {
     {
         return true;
     }
+    public int CalculatePoints()
+    {
+        int points = 0;
+        return points;
+    }
     private Boolean SlotColorRule()
     {
         return true;
@@ -36,4 +52,6 @@ public class RuleHandler {
         return true;
     }
     //private array GetNeighbors()
+    //SET of functions for each group/personal goal card
+    //Functions for point counting
 }
