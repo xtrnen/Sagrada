5362d8e90236b188c3329d78c718278b47987862
xtrnen <jtrnenyx@gmail.com>
2020-02-12 11:10:09 +0100

    Half of iCalculate functions implemented without being tested

--- app/src/main/java/Model/Points/Quests/Quest.java
+++ app/src/main/java/Model/Points/Quests/Quest.java
@@ -1,10 +1,20 @@
 package Model.Points.Quests;
 
+import java.util.Arrays;
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+
 import Model.Structs.Dice;
 
 public class Quest {
     IQuestCalculate personalQCalculator;
     IQuestCalculate commonQCalculator;
+    public int columns = 5;
+    public int rows = 4;
+
+    private String[] colorsArray = {"RED", "GREEN", "BLUE", "YELLOW", "VIOLET"};
 
     public void SetPersonalCalculator(PQ_TYPES qType)
     {
@@ -85,90 +95,158 @@ public class Quest {
                 iCalculate = (Dice[] dices) -> {
                     //TODO: Implement
                     int points = 0;
+                    String[] colors = {};
 
+                    for(int col = 0; col < columns; col++){
+                        for(int row = 0; row < rows; row++){
 
-
-                    return 0;
+                        }
+                    }
+                    //for each column
+                        //for each row
+                            //take dice[row][col]
+                            //if not exist -> break
+                            //Check if dice color is not already in array
+                            //if not add the color to array
+                            //else break
+                        //compare array and refArray -> points += 5;
+                        //reset bools
+                    return points;
                 };
                 break;
             case DIFF_COLUMN:
                 iCalculate = (Dice[] dices) -> {
                     //TODO: Implement
                     int points = 0;
-
-
-
-                    return 0;
+                    int[] numbers;
+                    //for each column
+                        //for each row
+                            //take dice[row][col]
+                            //if not exist -> break
+                            //check if dice number is not already in array
+                            //if not add number to array
+                            //else break
+                        //if array size == MAX_ROW_SIZE -> points += 4
+                        //reset array
+                    return points;
                 };
                 break;
             case RAINBOW_ROW:
                 iCalculate = (Dice[] dices) -> {
                     //TODO: Implement
                     int points = 0;
+                    String[] colors = {};
+                    //for each row
+                        //for each column
+                            //take dice
+                            //if not exist -> break
+                            //check color in not in array else break
+                            //add to array
+                        //compare array and ref array -> points += 6;
+                        //reset array
 
-
-
-                    return 0;
+                    return points;
                 };
                 break;
             case DIFF_ROW:
                 iCalculate = (Dice[] dices) -> {
                     //TODO: Implement
                     int points = 0;
-
-
-
-                    return 0;
+                    int[] numbers;
+                    //for each row
+                        //for each col
+                            //take dice[row][col]
+                            //if not exist -> break
+                            //check if dice number is not already in array
+                            //if not add number to array
+                            //else break
+                        //if array size == MAX_ROW_SIZE -> points += 5
+                        //reset array
+                    return points;
                 };
                 break;
             case LIGHT_PAIR:
                 iCalculate = (Dice[] dices) -> {
-                    //TODO: Implement
+                    //TODO: Test
                     int points = 0;
 
+                    int occurence1 = FindNumberOccurence(1, dices);
+                    int occurence2 = FindNumberOccurence(2, dices);
 
+                    points += (occurence1 <= occurence2) ? occurence1 * 2 : occurence2 * 2;
 
-                    return 0;
+                    return points;
                 };
                 break;
             case MIDDLE_PAIR:
                 iCalculate = (Dice[] dices) -> {
-                    //TODO: Implement
+                    //TODO: Test
                     int points = 0;
 
+                    int occurence1 = FindNumberOccurence(3, dices);
+                    int occurence2 = FindNumberOccurence(4, dices);
 
+                    points += (occurence1 <= occurence2) ? occurence1 * 2 : occurence2 * 2;
 
-                    return 0;
+                    return points;
                 };
                 break;
             case DARK_PAIR:
                 iCalculate = (Dice[] dices) -> {
-                    //TODO: Implement
+                    //TODO: Test
                     int points = 0;
 
+                    int occurence1 = FindNumberOccurence(5, dices);
+                    int occurence2 = FindNumberOccurence(6, dices);
 
+                    points += (occurence1 <= occurence2) ? occurence1 * 2 : occurence2 * 2;
 
-                    return 0;
+                    return points;
                 };
                 break;
             case ALL_COLOR:
                 iCalculate = (Dice[] dices) -> {
-                    //TODO: Implement
+                    //TODO: Test
                     int points = 0;
+                    int[] colors = new int[5];
 
+                    colors[0] = FindColorOccurence("RED", dices);
+                    colors[1] = FindColorOccurence("GREEN", dices);
+                    colors[2] = FindColorOccurence("BLUE", dices);
+                    colors[3] = FindColorOccurence("YELLOW", dices);
+                    colors[4] = FindColorOccurence("VIOLET", dices);
 
+                    if(colors[0] == 0 || colors[1] == 0 || colors[2] == 0 || colors[3] == 0 || colors[4] ==0)
+                        return points;
 
-                    return 0;
+                    Arrays.sort(colors);
+
+                    points += colors[0] * 4;
+
+                    return points;
                 };
                 break;
             case ALL_NUMBER:
                 iCalculate = (Dice[] dices) -> {
-                    //TODO: Implement
+                    //TODO: Test
                     int points = 0;
+                    int[] number = new int[6];
 
+                    number[0] = FindNumberOccurence(1, dices);
+                    number[1] = FindNumberOccurence(2, dices);
+                    number[2] = FindNumberOccurence(3, dices);
+                    number[3] = FindNumberOccurence(4, dices);
+                    number[4] = FindNumberOccurence(5, dices);
+                    number[5] = FindNumberOccurence(6, dices);
 
+                    if(number[0] == 0 || number[1] == 0 || number[2] == 0 || number[3] == 0 || number[4] == 0 || number[5] == 0)
+                        return points;
 
-                    return 0;
+                    Arrays.sort(number);
+
+                    points += number[0] * 5;
+
+                    return points;
                 };
                 break;
             case SAME_DIAGONAL:
@@ -176,6 +254,7 @@ public class Quest {
                     //TODO: Implement
                     int points = 0;
 
+                    //for each dice
 
 
                     return 0;
@@ -187,6 +266,26 @@ public class Quest {
                 break;
         }
         this.commonQCalculator = iCalculate;
+    }
+
+    private int FindNumberOccurence(int number, Dice[] dices)
+    {
+        int occurence = 0;
+        for(Dice dice : dices){
+            if(dice.number == number)
+                occurence++;
+        }
+        return occurence;
+    }
+
+    private int FindColorOccurence(String color, Dice[] dices)
+    {
+        int occurence = 0;
+        for(Dice dice : dices){
+            if(dice.color.equals(color.toUpperCase()))
+                occurence++;
+        }
+        return occurence;
     }
 
     public int RunEvaluation(Dice[] dices)
