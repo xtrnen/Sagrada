c402f3436a0718219d9f05ffb7c61420be5034ca
xtrnen <jtrnenyx@gmail.com>
2020-03-23 12:36:36 +0100

    Added MenuActivity with it's layout Buttons have onClickListeners with basic Log operation into commandLine

---
+++ app/src/main/java/com/example/sagrada/MenuActivity.java
@@ -0,0 +1,49 @@
+package com.example.sagrada;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MenuActivity extends AppCompatActivity {
+    private Button startGameButton;
+    private Button settingsButton;
+    private Button quitAppButton;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_layout);
+
+        //Bind layout buttons with class props & check binding
+        startGameButton = (Button) findViewById(R.id.StartGameButton);
+        assert startGameButton != null;
+        settingsButton = (Button) findViewById(R.id.SettingsButton);
+        assert settingsButton != null;
+        quitAppButton = (Button) findViewById(R.id.QuitAppButton);
+        assert quitAppButton != null;
+
+        //Set listeners
+        startGameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.println(Log.INFO, "StartGameListener", "Starting game");
+            }
+        });
+
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.println(Log.INFO, "StartSettingsListener", "Settings");
+            }
+        });
+
+        quitAppButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.println(Log.INFO, "StartQuitAppListener", "Quit App");
+            }
+        });
+    }
+}
