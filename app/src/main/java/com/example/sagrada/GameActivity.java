b652bdf8293f19087f6e3b2ba3d20dfc0301232b
xtrnen <jtrnenyx@gmail.com>
2020-03-23 15:41:56 +0100

    Game Activity files created.

---
+++ app/src/main/java/com/example/sagrada/GameActivity.java
@@ -0,0 +1,12 @@
+package com.example.sagrada;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GameActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
